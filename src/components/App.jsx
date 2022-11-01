ac20eb387b6414ca9d6fa8ed6169473693b4636c
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2022-11-02 00:01:24 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { Filter } from './Filter/Filter';
 import { PhoneBook, InformationArea } from './App.styled';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -46,6 +41,18 @@ export class App extends Component {
       contacts: newContacts,
     });
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parceConatacts = JSON.parse(contacts);
+    if (parceConatacts) {
+      this.setState({ contacts: parceConatacts });
+    }
+  }
+  componentDidUpdate(preProps, PrevState) {
+    if (this.state.contacts !== PrevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   render() {
     const contactsLength = this.state.contacts.length;
     return (
